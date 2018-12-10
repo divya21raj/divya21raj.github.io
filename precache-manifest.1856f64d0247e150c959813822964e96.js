7d712d897d4a2101a678a669220187d27569c584
Divya Raj <divya21raj@gmail.com>
2018-12-10 22:26:25 +0530

    Updates

---
+++ precache-manifest.1856f64d0247e150c959813822964e96.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "09a489a7f326d3af135c",
+    "url": "/static/css/main.2d4dfd2c.chunk.css"
+  },
+  {
+    "revision": "09a489a7f326d3af135c",
+    "url": "/static/js/main.09a489a7.chunk.js"
+  },
+  {
+    "revision": "e8fa598dde066704d923",
+    "url": "/static/js/1.e8fa598d.chunk.js"
+  },
+  {
+    "revision": "229c360febb4351a89df",
+    "url": "/static/js/runtime~main.229c360f.js"
+  },
+  {
+    "revision": "f5d4338961cfb49b08a1b4f8082e94f6",
+    "url": "/index.html"
+  }
+];
