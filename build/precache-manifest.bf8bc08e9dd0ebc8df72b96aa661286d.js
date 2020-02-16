0a658e88e1eefb04fd86547ed790a0bdc6cb657d
Divya Raj <divya21raj@gmail.com>
2020-02-16 19:15:43 +0530

    Delete CNAME

---
+++ build/precache-manifest.bf8bc08e9dd0ebc8df72b96aa661286d.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "229c360febb4351a89df",
+    "url": "/static/js/runtime~main.229c360f.js"
+  },
+  {
+    "revision": "b1026dbac7f42e1fe39d",
+    "url": "/static/js/main.b1026dba.chunk.js"
+  },
+  {
+    "revision": "2b737b2674a4f966917c",
+    "url": "/static/js/1.2b737b26.chunk.js"
+  },
+  {
+    "revision": "b1026dbac7f42e1fe39d",
+    "url": "/static/css/main.2d4dfd2c.chunk.css"
+  },
+  {
+    "revision": "3e339983e9fc7002d7272da5ac9e2c2d",
+    "url": "/index.html"
+  }
+];
