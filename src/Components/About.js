657f6d32bcd21d3162f59efc7738b9faf396691b
Divya Raj <div21devel@gmail.com>
2020-03-08 19:19:48 +0530

    Removed Address

--- src/Components/About.js
+++ src/Components/About.js
@@ -4,13 +4,13 @@ class About extends Component {
   render() {
 
     if(this.props.data){
-      var name = this.props.data.name;
+      //var name = this.props.data.name;
       var profilepic= "images/"+this.props.data.image;
       var bio = this.props.data.bio;
-      var street = this.props.data.address.street;
-      var city = this.props.data.address.city;
-      var state = this.props.data.address.state;
-      var zip = this.props.data.address.zip;
+      // var street = this.props.data.address.street;
+      // var city = this.props.data.address.city;
+      // var state = this.props.data.address.state;
+      // var zip = this.props.data.address.zip;
       var phone= this.props.data.phone;
       var email = this.props.data.email;
       var resumeDownload = this.props.data.resumedownload;
@@ -30,11 +30,7 @@ class About extends Component {
                <div className="columns contact-details">
                   <h2>Contact Details</h2>
                   <p className="address">
-						   <span>{name}</span><br />
-						   <span>{street}<br />
-						         {city}, {state}, {zip}
-                   </span><br />
-						   <span>{phone}</span><br />
+                     <span>{phone}</span><br />
                      <span>{email}</span>
 					   </p>
                </div>
